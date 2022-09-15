339b1831d51d2b24dcdd6f2688b0160428cae063
Guilherme Venturim <guilhermeventurim@outlook.com>
2022-09-15 16:16:04 -0300

    Estrutura inicial

---
+++ M2/tiposDeDadosPrimitivos/index.js
@@ -0,0 +1,6 @@
+const nomeSobrenome = `Guilherme Venturim`   //String
+const numbPreferido = 1911                   //Number
+let nomePai                                  //Undefined = Valor a ser definido posteriormente
+let sobrenomePai = null                      //Nulo      = Não aponto para nenhum lugar da memória
+const solteiro = true                        //Booleano  = Podendo ser verdadeiro ou falso (valor lógico)
+
